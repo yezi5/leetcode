41bfe5936130ff168033576306d0fa795a5a0f23
叶子 <2693820285@qq.com>
2020-12-12 19:37:45 +0800

    新增 上到下打印二叉树 II

---
+++ src/main/java/icu/yezi/leetcode/l树/l从上到下打印二叉树II/Solution.java
@@ -0,0 +1,54 @@
+package icu.yezi.leetcode.l树.l从上到下打印二叉树II;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @author 叶子
+ * @Description https://leetcode-cn.com/problems/cong-shang-dao-xia-da-yin-er-cha-shu-ii-lcof/
+ * @DevelopmentTools IntelliJ IDEA
+ * @PackageName icu.yezi.leetcode.l树.l从上到下打印二叉树II
+ * @Data 2020/12/12 星期六 19:10
+ */
+public class Solution {
+    /**
+     * 广度优先算法
+     * 队列实现
+     *
+     * 参考了LeetCode题解
+     * @param root
+     * @return
+     */
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> listList = new ArrayList<>();
+        if (root==null){return listList;}
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()){
+            List<Integer> list = new ArrayList<>();
+
+            /**
+             * 注意，这里的循环设置的很巧妙
+             * i初始化值为当前层含有的结点数
+             * 循环中queue新增的数据量不影响循环次数
+             */
+            for (int i = queue.size(); i > 0; i--) {
+                TreeNode cure = queue.poll();
+                list.add(cure.val);
+                if (cure.left!=null){
+                    queue.add(cure.left);
+                }
+                if (cure.right!=null){
+                    queue.add(cure.right);
+                }
+            }
+            listList.add(list);
+        }
+
+        return listList;
+    }
+}
+
---
+++ src/main/java/icu/yezi/leetcode/l树/l从上到下打印二叉树II/TreeNode.java
@@ -0,0 +1,15 @@
+package icu.yezi.leetcode.l树.l从上到下打印二叉树II;
+
+/**
+ * @author 叶子
+ * @Description 请设置
+ * @DevelopmentTools IntelliJ IDEA
+ * @PackageName icu.yezi.leetcode.l树.l从上到下打印二叉树II
+ * @Data 2020/12/12 星期六 19:10
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
