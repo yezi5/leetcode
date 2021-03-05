438f4397dfcb659eef6a10ffb9d6b3f4f230c5cc
叶子 <2693830285@qq.com>
2021-03-05 20:05:44 +0800

    相同的树

--- src/main/java/icu/yezi/dataStructure/tree/l普通的树/基础算法.java
+++ src/main/java/icu/yezi/dataStructure/tree/l普通的树/基础算法.java
@@ -18,4 +18,22 @@ public class 基础算法 {
         if (root == null) return 0;
         return Math.max(depth(root.left),depth(root.right)) + 1;
     }
+
+    /**
+     * 二叉树最近公共祖先
+     * @param root
+     * @param p
+     * @param q
+     * @return
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null || root == p || root == q) return root;
+
+        TreeNode left = lowestCommonAncestor(root.left,p,q);
+        TreeNode right = lowestCommonAncestor(root.right,p,q);
+        if(left == null) return right;
+        if(right == null) return left;
+
+        return root;
+    }
 }
